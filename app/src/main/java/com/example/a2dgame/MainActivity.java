104b47ee09b01d4388aa2d6f8e70209ed1b1197b
MasaNakura <ronaldoasmalol@gmail.com>
2021-03-14 22:07:40 -0700

    Game loop

--- app/src/main/java/com/example/a2dgame/MainActivity.java
+++ app/src/main/java/com/example/a2dgame/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.a2dgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.Window;
+import android.view.WindowManager;
 
+/*
+    MainActivity is the entry point to our app.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // window to fullscreen
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Set content view to game, so that objects in the game class can be rendered to the screen
+        setContentView(new Game(this));
     }
 }
